6805a50ec36ae6820c73d90153dd1041b1392e52
Antonio Fernando dos Santos Guedes <aguedes2@gmail.com>
2022-09-25 20:28:58 -0300

    simplificar método de jogada do player. transformar 3 métodos em 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-const btnPedra = document.querySelector('.btn-pedra')
 const player = document.querySelector('.jogada-player')
 const cpu = document.querySelector('.jogada-cpu')
 const footer = document.querySelector('.footer')
@@ -10,30 +9,39 @@ var opcoes = ['pedra', 'papel', 'tesoura']
 var opcaoP = ''
 var opcaoComp = ''
 
-function jogarPedra() {
-  img.src = `../imgs/${opcoes[0]}.png`
-  player.appendChild(img)
-  jogadaCPU()
-  verificarVencedor(opcoes[0], opcoes[jogadaCPU()])
-}
+// function jogarPedra() {
+//   img.src = `./imgs/${opcoes[0]}.png`
+//   player.appendChild(img)
+//   jogadaCPU()
+//   verificarVencedor(opcoes[0], opcoes[jogadaCPU()])
+// }
 
-function jogarPapel() {
-  img.src = `../imgs/${opcoes[1]}.png`
-  player.appendChild(img)
-  jogadaCPU()
-  verificarVencedor(opcoes[1], opcoes[jogadaCPU()])
-}
+// function jogarPapel() {
+//   img.src = `./imgs/${opcoes[1]}.png`
+//   player.appendChild(img)
+//   jogadaCPU()
+//   verificarVencedor(opcoes[1], opcoes[jogadaCPU()])
+// }
 
-function jogarTesoura() {
-  img.src = `../imgs/${opcoes[2]}.png`
+// function jogarTesoura() {
+//   img.src = `./imgs/${opcoes[2]}.png`
+//   player.appendChild(img)
+//   jogadaCPU()
+//   verificarVencedor(opcoes[2], opcoes[jogadaCPU()])
+// }
+
+//Substitui 3 métodos por 1.
+
+function jogar(jogada) {
+  img.src = `./imgs/${jogada}.png`
   player.appendChild(img)
   jogadaCPU()
-  verificarVencedor(opcoes[2], opcoes[jogadaCPU()])
+  verificarVencedor(jogada, opcoes[jogadaCPU()])
 }
 
 function jogadaCPU() {
   let opcaoCPU = Math.round(Math.random() * 2)
-  imgCPU.src = `../imgs/${opcoes[opcaoCPU]}.png`
+  imgCPU.src = `./imgs/${opcoes[opcaoCPU]}.png`
   cpu.appendChild(imgCPU)
   return opcaoCPU
 }
@@ -43,6 +51,7 @@ function verificarVencedor(p, cpu) {
   let cpuVenceu = 'CPU Venceu!'
   let empate = 'Empatou!'
   let msg = ''
+
   switch (p) {
     case 'papel':
       if (cpu == 'pedra') {
